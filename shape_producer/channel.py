b374eff02a5ab6a1ed6944ceb56e489714dbf5d7
Sebastian Brommer <sebastian.brommer@student.kit.edu>
2019-05-31 09:24:51 +0200

    update tt 2016 selection to new tauid

--- shape_producer/channel.py
+++ shape_producer/channel.py
@@ -137,8 +137,7 @@ class MTSM2016(Channel):
             Cut("iso_1<0.15", "muon_iso"), 
             Cut("q_1*q_2<0", "os"),
             Cut("mt_1<50", "m_t"),
-            Cut("((trg_singlemuon==1 && pt_1>23 && pt_2>30) + (trg_mutaucross==1 && pt_1>20 && pt_1<=23 && pt_2>30))",
-                "trg_singlemuoncross")
+            Cut("pt_2>30 && ((trg_singlemuon== 1) || ((trg_mutaucross == 1) && pt_1>=23))","trg_singlemuoncross")
             )
 
 
@@ -223,11 +222,11 @@ class ETSM2016(Channel):
             Cut("againstMuonLoose3_2>0.5", "againstMuonDiscriminator"),
             Cut("againstElectronTightMVA6_2>0.5",
                 "againstElectronDiscriminator"),
-            Cut("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
-            Cut("iso_1<0.1", "ele_iso"), Cut("q_1*q_2<0", "os"),
+            Cut("byTightIsolationMVArun2017v2DBoldDMwLT2017_2>0.5", "tau_iso"),
+            Cut("iso_1<0.1", "ele_iso"), 
+            Cut("q_1*q_2<0", "os"), 
             Cut("mt_1<50", "m_t"),
-            Cut("(trg_singleelectron==1 && pt_1>26 && pt_2>30)",
-                "trg_singleelectron"))
+            Cut("(trg_singleelectron==1 && pt_1>26 && pt_2>30)","trg_singleelectron"))
 
 
 class TT(Channel):
@@ -258,10 +257,11 @@ class TTSM2016(Channel):
                 "againstMuonDiscriminator"),
             Cut("againstElectronVLooseMVA6_1>0.5 && againstElectronVLooseMVA6_2>0.5",
                 "againstElectronDiscriminator"),
-            Cut("byTightIsolationMVArun2v1DBoldDMwLT_1>0.5", "tau_1_iso"),
-            Cut("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_2_iso"),
-            Cut("q_1*q_2<0", "os"), Cut("pt_tt>50", "pt_h"),
-            Cut("(trg_doubletau==1 && pt_1>40 && pt_2>40)", "trg_doubletau"))
+            Cut("byTightIsolationMVArun2017v2DBoldDMwLT2017_1>0.5", "tau_1_iso"),
+            Cut("byTightIsolationMVArun2017v2DBoldDMwLT2017_2>0.5", "tau_2_iso"),
+            Cut("q_1*q_2<0", "os"), 
+            Cut("pt_tt>50", "pt_h"),
+            Cut("trg_doubletau==1", "trg_doubletau"))
 
 
 class TTMSSM2017(Channel):
